4430541b4df9462c0f7d413cfce64dabc65f87f7
opticrom <alexanderturco1@gmail.com>
2023-04-26 18:40:00 -0400

    resume update

--- components/navbar.js
+++ components/navbar.js
@@ -23,7 +23,7 @@ export const Navbar = () => {
               </Link>
             </li>
             <li>
-              <Link href="/resume_2023.pdf">
+              <Link href="/alexander_turco_resume_apr2023.pdf">
               <a class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Resume/CV</a>
               </Link>
             </li>
