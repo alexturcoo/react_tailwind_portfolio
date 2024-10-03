96988bdc4b7d68153f430692a388ecdcd08374a2
alexturcoo <alexanderturco1@gmail.com>
2024-10-03 16:29:09 -0400

    updates

--- pages/contact.js
+++ pages/contact.js
@@ -8,7 +8,7 @@ export default function Contact(){
             <h3 className=" font-sans text-5xl pb-10 text-teal-600 text-center">How to Contact me</h3>
             <hr className="my-4 mx-auto w-48 h-1 bg-gray-100 rounded border-0 md:my-10 dark:bg-gray-700" />
                 <ul className="py-5 text-center">
-                <label className=" font-mono bg-gradient-to-r from-cyan-500 text- to-teal-500 text-black p-2 border-none rounded ">School Email: turcoa1@mcmaster.ca</label>
+                <label className=" font-mono bg-gradient-to-r from-cyan-500 text- to-teal-500 text-black p-2 border-none rounded ">School Email: alexander.turco@mail.utoronto.ca</label>
                 </ul>
                 <ul className="py-5 text-center">
                 <label className=" font-mono bg-gradient-to-r from-cyan-500 text- to-teal-500 text-black p-2 border-none rounded ">Personal Email: alexanderturco1@gmail.com</label>
